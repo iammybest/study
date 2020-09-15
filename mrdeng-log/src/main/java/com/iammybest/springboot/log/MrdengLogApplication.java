6182b4a44eef6259953160c9ae743ce7e0a27e94
iammybest <396187665@qq.com>
2020-09-15 21:37:00 +0800

    log

---
+++ mrdeng-log/src/main/java/com/iammybest/springboot/log/MrdengLogApplication.java
@@ -0,0 +1,13 @@
+package com.iammybest.springboot.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrdengLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrdengLogApplication.class, args);
+    }
+
+}
