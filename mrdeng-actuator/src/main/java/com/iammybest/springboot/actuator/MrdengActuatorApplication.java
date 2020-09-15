abcffa0e7e8588faf9de82559160cfa43cbdf0b8
iammybest <396187665@qq.com>
2020-09-15 22:12:46 +0800

    log and actuator

---
+++ mrdeng-actuator/src/main/java/com/iammybest/springboot/actuator/MrdengActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.iammybest.springboot.actuator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class MrdengActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrdengActuatorApplication.class, args);
+        log.debug("*********SpringBoot Actuator**********");
+    }
+
+}
