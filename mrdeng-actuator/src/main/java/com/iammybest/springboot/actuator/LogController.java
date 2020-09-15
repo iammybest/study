fc9ef396348a51dc9147da1b9586ebd5c2c85c91
iammybest <396187665@qq.com>
2020-09-15 22:50:37 +0800

    log and actuator

---
+++ mrdeng-actuator/src/main/java/com/iammybest/springboot/actuator/LogController.java
@@ -0,0 +1,22 @@
+package com.iammybest.springboot.actuator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("log")
+@Slf4j
+public class LogController {
+    @GetMapping("print")
+    public String printLog(@RequestParam("context")String context){
+        log.trace("trace:{}",context);
+        log.debug("debug:{}",context);
+        log.info("info:{}",context);
+        log.warn("warn:{}",context);
+        log.error("error:{}",context);
+        return context;
+    }
+}
--- mrdeng-actuator/src/main/java/com/iammybest/springboot/actuator/MrdengActuatorApplication.java
+++ mrdeng-actuator/src/main/java/com/iammybest/springboot/actuator/MrdengActuatorApplication.java
@@ -3,8 +3,10 @@ package com.iammybest.springboot.actuator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @Slf4j
 public class MrdengActuatorApplication {
 
